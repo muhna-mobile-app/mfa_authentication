cf72115c8262713cc3f652ddc9770a2814495f9b
kaio-nink <kaionink@gmail.com>
2023-04-27 12:00:25 -0300

    test: add AuthRemoteDataSource test for postRegister

--- test/auth_remote_datasource_test.dart
+++ test/auth_remote_datasource_test.dart
@@ -3,7 +3,17 @@ import 'package:mfa_authentication/app/data/datasources/remote/auth_remote_datas
 import 'package:mfa_authentication/app/data/models/auth_token_model.dart';
 
 void main() {
-  var loginData = {"email": "mail@mail.com", "password": "123"};
+  var loginData = {
+    "email": "mail@mail.com",
+    "password": "123",
+  };
+  var registerData = {
+    "name": "kaio",
+    "email": "kaion1@mail.com",
+    "password": "1234",
+    "roles": ["user"],
+  };
+
   test('It should return an AuthTokenModel', () async {
     AuthRemoteDataSourceImp authRemoteDataSource = AuthRemoteDataSourceImp();
 
@@ -18,5 +28,12 @@ void main() {
     final response = await authRemoteDataSource.postLogin(loginData);
     final authToken = response.token;
     expect(authToken, isA<String>());
+  });
+
+  test('It should register an user and return true', () async {
+    AuthRemoteDataSourceImp authRemoteDataSource = AuthRemoteDataSourceImp();
+
+    final response = await authRemoteDataSource.postRegister(registerData);
+    expect(response, true);
   });
 }
