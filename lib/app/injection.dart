027800228490dc4306475bfc5b392971b4fdf943
kaio-nink <kaionink@gmail.com>
2023-04-27 11:55:31 -0300

    feat: update dependency injections, and add Stores singletons

--- lib/app/injection.dart
+++ lib/app/injection.dart
@@ -6,16 +6,17 @@ import 'package:mfa_authentication/app/data/repositories/auth_remote_repository_
 import 'package:mfa_authentication/app/domain/repositories/auth_local_repository.dart';
 import 'package:mfa_authentication/app/domain/repositories/auth_remote_repository.dart';
 import 'package:mfa_authentication/app/domain/usecases/login_user/login_user_usecase.dart';
+import 'package:mfa_authentication/app/domain/usecases/register_user/register_user_usecase.dart';
 import 'package:mfa_authentication/app/presentation/controllers/login_controller.dart';
 
 import 'data/datasources/local/auth_local_datasource.dart';
 import 'data/datasources/local/auth_local_datasource_imp.dart';
 import 'domain/usecases/auth_token/auth_token_usecase.dart';
+import 'presentation/stores/auth_store.dart';
+import 'presentation/stores/register_form_store.dart';
 
 class Inject {
-  static void init() {
-    GetIt getIt = GetIt.instance;
-
+  static void init(GetIt getIt) {
     // datasources
     getIt.registerLazySingleton<AuthRemoteDataSource>(
       () => AuthRemoteDataSourceImp(),
@@ -33,9 +34,15 @@ class Inject {
     );
 
     // useCases
-    getIt.registerLazySingleton(() => LogInUserUseCase(getIt()));
-    getIt.registerLazySingleton(() => AuthTokenUseCase(getIt()));
+    getIt.registerSingleton<LogInUserUseCase>(LogInUserUseCase(getIt()));
+    getIt.registerSingleton<AuthTokenUseCase>(AuthTokenUseCase(getIt()));
+    getIt.registerSingleton<RegisterUserUseCase>(RegisterUserUseCase(getIt()));
+
     //controllers
-    getIt.registerLazySingleton(() => LoginController(getIt(), getIt()));
+    getIt.registerLazySingleton(() => LoginController());
+
+    //Stores
+    getIt.registerSingleton<RegisterFormStore>(RegisterFormStore());
+    getIt.registerSingleton<AuthStore>(AuthStore());
   }
 }
